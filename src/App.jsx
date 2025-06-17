077bd4cf909c08dbff68e0025b549a168ae90575
jamminP <parkjaemin08@gmail.com>
2025-06-17 14:57:17 +0900

    ✨ Feat: React-Router-Dom 라우팅 구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
-import "./App.css";
-import MovieCard from "./components/MovieCard";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import MovieDetail from "./components/MovieDetail";
-import MOVIELISTDATA from "./data/movieListData.json";
 
 function App() {
   return (
-    <>
-      <MovieDetail />
-      {/* <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 p-4">
-        {MOVIELISTDATA.results.map((movie) => (
-          <MovieCard
-            key={movie.id}
-            title={movie.title}
-            posterPath={movie.poster_path}
-            voteAverage={movie.vote_average}
-          />
-        ))}
-      </div> */}
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/details" element={<MovieDetail />} />
+    </Routes>
   );
 }
 
